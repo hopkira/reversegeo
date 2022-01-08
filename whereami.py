600f53bf78c7fdbdee8a22cbc70aca17a93eca33
Richard Hopkins <hopkira@googlemail.com>
2022-01-08 21:26:44 +0000

    Update whereami.py

--- whereami.py
+++ whereami.py
@@ -1,3 +1,9 @@
+import googlemaps
+import json
 from secrets import *
 
-print(key)
+gmaps = googlemaps.Client(key=key)
+
+address = gmaps.reverse_geocode((54.5041124, -1.3408161), result_type=["route"])
+
+print("I am located in",address[0]["address_components"][1]["long_name"],"in",address[0]["address_components"][2]["long_name"])
